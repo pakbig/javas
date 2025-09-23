923af4dce7b3ff2f43ce97f159a6749a4ff83b08
pakbig <masbig.com>
2025-09-23 14:49:08 +0700

    bikin folder scripts dan app.js

---
+++ learn60/scripts/app.js
@@ -0,0 +1,5 @@
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "darkblue";
+        document.body.style.fontFamily = "Arial, sans-serif";
+        document.body.style.textAlign = "center";
+        document.body.style.paddingTop = "50px";
